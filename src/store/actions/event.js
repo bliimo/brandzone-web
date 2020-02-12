b9839ed7703510b907a2b40cd9daa46f69274f34
Erwin Doria <erwin.doria@bliimo.com>
2020-02-12 10:44:14 +0800

    fixed show booking

--- src/store/actions/event.js
+++ src/store/actions/event.js
@@ -6,7 +6,7 @@ import { API } from '../../constants/api';
 export const getLatestEvents = id => dispatch => {
   dispatch({ type: EVENTS_REQUEST });
   axios
-    .get(`${API}/event/latest${id ? `?userId=${id}` : ''}`)
+    .get(`${API}/event/latest${id || localStorage.getItem('userType') ? `?userId=${id}` : ''}`)
     .then(res => {
       dispatch({
         type: EVENTS_SUCCESS,
